535601f3f230fe02ed5f1e1f476e04a4794c9f67
“Reiwen <puby0911@gmail.com>
2020-11-08 14:52:21 +0800

    clean

--- build/hello_pkg/CMakeFiles/hello_pkg_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hello_pkg_generate_messages_lisp"
-  "/home/angelo/rostest_ws/devel/share/common-lisp/ros/hello_pkg/srv/AddTwoInts.lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hello_pkg_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- devel/share/common-lisp/ros/hello_pkg/srv/_package_AddTwoInts.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package hello_pkg-srv)
-(cl:export '(A-VAL
-          A
-          B-VAL
-          B
-          SUM-VAL
-          SUM
-))
